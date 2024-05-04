561b0174c884c6b12221d5054ea6bb9bace185fe
Ciekoski <163067249+Ciekoski@users.noreply.github.com>
2024-05-04 14:46:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const login =document.querySelector('#login');
+const senha=document.querySelector('#senha');
+const forms = document.querySelector('#formulario');
+
+forms.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if(login.value === ""){
+        alert("Faltou o seu login!");
+        return;
+    }
+    if(senha.value ===""){
+        alert("Faltou a sua senha!");
+        return;
+    }
+    forms.submit();
+})
+
+
+
+
+
